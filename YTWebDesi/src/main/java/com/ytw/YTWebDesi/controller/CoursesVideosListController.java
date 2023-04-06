670f2523d1ce159442c0cae3ff0ac5b85a63c106
Muni <Lumen.com>
2023-04-06 08:47:39 +0530

    Course Video CRUD : Muni

--- YTWebDesi/src/main/java/com/ytw/YTWebDesi/controller/CoursesVideosListController.java
+++ YTWebDesi/src/main/java/com/ytw/YTWebDesi/controller/CoursesVideosListController.java
@@ -2,18 +2,20 @@ package com.ytw.YTWebDesi.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ytw.YTWebDesi.model.CourseDetail;
 import com.ytw.YTWebDesi.model.CoursesVideosList;
 import com.ytw.YTWebDesi.repository.CoursesVideosListRepository;
 
@@ -47,4 +49,71 @@ public class CoursesVideosListController {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+	
+	// Update Specific Course Video Using VideoId
+	@PutMapping("/update/specificVideo/{videoId}")
+	public ResponseEntity<CoursesVideosList> updateSpecificCourseVideo(@PathVariable("videoId") int videoId,@RequestBody CoursesVideosList coursesVideo) {
+		Optional<CoursesVideosList> coursesVideoDb=coursesVideosListRepository.findById(videoId);
+		try {
+			if(coursesVideoDb.isPresent()) {
+				CoursesVideosList _coursesVideoDb=coursesVideoDb.get();
+				_coursesVideoDb.setCourseId(coursesVideo.getCourseId());
+				_coursesVideoDb.setYtVideoId(coursesVideo.getYtVideoId());
+				_coursesVideoDb.setVideoTitle(coursesVideo.getVideoTitle());
+				_coursesVideoDb.setIsPaid(coursesVideo.getIsPaid());
+				_coursesVideoDb.setVideoFee(coursesVideo.getVideoFee());
+				_coursesVideoDb.setLastUpdatedDateTime(new Date());
+				
+				// Saving Updated Video Details To Database
+				CoursesVideosList _video=coursesVideosListRepository.save(_coursesVideoDb);
+				return new ResponseEntity<>(_video,HttpStatus.OK);
+			}else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}catch (Exception e) {
+		      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	// Delete Specific Course Video Using VideoId
+	@DeleteMapping("delete/video/{videoId}")
+	public ResponseEntity<HttpStatus> deleteSpecificCourseVideo(@PathVariable("videoId") int videoId) {
+		try {
+			coursesVideosListRepository.deleteById(videoId);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	    } catch (Exception e) {
+	    	return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
+	
+	// Find Specific Course
+	@GetMapping("/search/video/{videoId}")
+	public ResponseEntity<CoursesVideosList> getVideoByVideoId(@PathVariable("videoId") int videoId) {
+		try {
+			Optional<CoursesVideosList> video = coursesVideosListRepository.findById(videoId);
+			if (video.isPresent()) {
+				CoursesVideosList _video=video.get();
+				return new ResponseEntity<>(_video, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+		}catch (Exception e) {
+		    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
